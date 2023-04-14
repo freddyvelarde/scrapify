b3a6ea427b9887d4ffbb8a1499692e28c127ab97
Freddy Velarde <freddyvelarde59@gmail.com>
2023-04-13 22:29:55 -0400

    :construction: adding scrapper endpoint

--- scrapify-api/scrapper/app.py
+++ scrapify-api/scrapper/app.py
@@ -1,11 +1,26 @@
 from flask import Flask, jsonify
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+
+
+@app.route("/search/<item>")
+def search(item):
+    url = f"https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw={item}&_sacat=0"
+
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    res = []
+    for link in soup.find_all("a"):
+        if link.get("class") is not None and link.get("class")[0] == "s-item__link":
+            res.append(link.get("href"))
+
+    return res
 
 
 @app.route("/")
 def index():
     return jsonify({"message": "hello world from docker and nginx"})
-
-
-app.run(debug=True, host="0.0.0.0", port=5000)
