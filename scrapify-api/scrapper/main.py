5c6002faacad2d00b648be3833615c4f7cac2014
Freddy Velarde <freddyvelarde59@gmail.com>
2023-04-13 21:53:17 -0400

    :sparkles: adding .dockerignore

--- scrapify-api/scrapper/main.py
+++ scrapify-api/scrapper/main.py
@@ -1,11 +1,4 @@
-from flask import Flask, jsonify
+from app import app
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return jsonify({"message": "hello world from docker and nginx"})
-
-
-app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
