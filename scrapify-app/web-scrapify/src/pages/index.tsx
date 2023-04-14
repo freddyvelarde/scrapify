e6a7bb5e71d02240a9338120ec05b870db45811e
Freddy Velarde <freddyvelarde59@gmail.com>
2023-04-14 15:34:50 -0400

    :lipstick: Nextjs web client already added.

---
+++ scrapify-app/web-scrapify/src/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="">
+      <form action="">
+        <input placeholder="hello world" type="password" />
+      </form>
+      <h1>hello world from nextjs</h1>
+    </main>
+  );
+}
