fd2ea98bfa62729eaba2ec6b4a8646e7cd5bdf34
Freddy Velarde <freddyvelarde59@gmail.com>
2023-04-16 17:08:15 -0400

    :sparkles: CORS added into `flask` and integration with  nextjs`

--- scrapify-api/scrapper/modules/__init__.py
+++ scrapify-api/scrapper/modules/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
 from modules.controllers.scrapping import scrapping_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
+
+CORS(
+    app,
+    resources={r"/api/*": {"origins": ["http://localhost:3000/"]}},
+)
 
 app.register_blueprint(scrapping_bp)
--- scrapify-app/web-scrapify/src/pages/index.tsx
+++ scrapify-app/web-scrapify/src/pages/index.tsx
@@ -1,10 +1,33 @@
 import HeadComponent from "@/components/Head";
+import { useState } from "react";
 
 export default function Home() {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handlerOnClick = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setSearchValue(e.target.value);
+
+  const getStoreData = async (item: string) => {
+    const request = await fetch(`http://localhost/api/scrapper/search/${item}`);
+    const jsonResponse = await request.json();
+    console.log(jsonResponse);
+  };
+
+  const handlerOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    getStoreData(searchValue);
+
+    // console.log(searchValue);
+  };
+
   return (
-    <main className="bg-color2">
+    <div className="bg-color2">
       <HeadComponent title="Scrapify" />
-      <h1>hello world from nextjs</h1>
-    </main>
+
+      <form action="" onSubmit={handlerOnSubmit}>
+        <input type="text" onChange={handlerOnClick} className="text-color1" />
+        <button>search</button>
+      </form>
+    </div>
   );
 }
