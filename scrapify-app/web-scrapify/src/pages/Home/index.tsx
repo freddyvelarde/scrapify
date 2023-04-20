fdb16b19a95b2e1b7460c9a9586f87e4fb2bcf14
Freddy Velarde <freddyvelarde59@gmail.com>
2023-04-19 21:36:25 -0400

    :recycle: Refactor some code into `nextjs` web-client

--- scrapify-app/web-scrapify/src/pages/Home/index.tsx
+++ scrapify-app/web-scrapify/src/pages/Home/index.tsx
@@ -1,29 +1,9 @@
-import { useState } from "react";
-import HeadComponent from "@/components/Head";
-import SearchForm from "./sections/SearchForm";
-import useStoreData from "@/hooks/useStoreData";
-import { ProductList } from "./sections/ProductList";
+import Products from "../Products";
 
 export default function HomeSection() {
-  const [searchValue, setSearchValue] = useState("");
-  const { getStoreData, storeData } = useStoreData();
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setSearchValue(e.target.value);
-
-  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    getStoreData(searchValue);
-  };
-
   return (
-    <div className="">
-      <HeadComponent title="Scrapify" />
-      <SearchForm
-        handleInputChange={handleInputChange}
-        handleFormSubmit={handleFormSubmit}
-      />
-      <ProductList products={storeData.data} />
+    <div>
+      <Products />
     </div>
   );
 }
