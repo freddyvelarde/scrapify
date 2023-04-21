fb583ea2e8d4663ffc068ebe0c304423e9b92131
Freddy Velarde <freddyvelarde59@gmail.com>
2023-04-21 11:27:12 -0400

    :alien: refactoring some for `/categories` endpoint

--- scrapify-api/scrapper/modules/controllers/store.py
+++ scrapify-api/scrapper/modules/controllers/store.py
@@ -7,13 +7,25 @@ from database.db_connection import redis_client
 scrapping_bp = Blueprint("scrapping", __name__)
 
 
+def get_categories_data(products):
+    products_most_bought = [
+        "bookcases",
+        "Mobile Electronics Accessories",
+        "LED lights",
+        "jewelry",
+    ]
+    for product in products_most_bought:
+        products.append({"product": product, "data": store_scrapper(product)})
+    redis_client.set("categories_cache", str(products))
+
+
 @scrapping_bp.route("/search/<product>")
 def search(product):
     try:
         products = store_scrapper(product)
 
         return make_response(jsonify(products), 200)
-    except ValueError as error:
+    except Exception as error:
         return make_response(
             jsonify({"message": "error getting products", "error": f"{error}"}), 500
         )
@@ -30,39 +42,18 @@ def categories():
         response_products = []
 
         if current_hour == 1 and current_minute == 15:  # 21:15 - (GMT-4)
-            products = [
-                "bookcases",
-                "Mobile Electronics Accessories",
-                "LED lights",
-                "jewelry",
-            ]
-            for product in products:
-                response_products.append(
-                    {"product": product, "data": store_scrapper(product)}
-                )
-            redis_client.set("categories_cache", str(response_products))
+            get_categories_data(response_products)
             return make_response(jsonify(response_products), 200)
 
         if cached_data is not None:
             return make_response(
                 jsonify(ast.literal_eval(str(cached_data.decode()))), 200
             )
-
-        products = [
-            "bookcases",
-            "Mobile Electronics Accessories",
-            "LED lights",
-            "jewelry",
-        ]
-        for product in products:
-            response_products.append(
-                {"product": product, "data": store_scrapper(product)}
-            )
-        redis_client.set("categories_cache", str(response_products))
+        get_categories_data(response_products)
 
         return make_response(jsonify(response_products), 200)
 
-    except ValueError as error:
+    except Exception as error:
         return make_response(
             jsonify({"message": "error getting products", "error": f"{error}"}), 500
         )
